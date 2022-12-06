7a662fe4ebe9d41e945d9b3f5a8a09134addda69
Cappy Ishihara <cappy@cappuchino.xyz>
2022-12-06 13:16:16 +0700

    day 4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 
 aoc_main::main! {
@@ -8,4 +9,5 @@ aoc_main::main! {
     day1 : generator => part_1, part_2;
     day2 : generator => part_1, part_2;
     day3 : generator => part_1, part_2;
+    day4 : generator => part_1, part_2;
 }
